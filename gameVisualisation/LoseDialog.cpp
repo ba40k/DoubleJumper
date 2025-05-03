c74fe00261753b8105ee18d144e6c6eb6ca29ae7
Radamir <dermeister20032132134@gmail.com>
2025-05-03 22:50:42 +0300

    сделал кнопки(пока нерабочие) в менюшке проигрыша

--- gameVisualisation/LoseDialog.cpp
+++ gameVisualisation/LoseDialog.cpp
@@ -5,39 +5,61 @@
 #include "LoseDialog.h"
 
 #include <iostream>
+#include <QPushButton>
 
-LoseDialog::LoseDialog(QWidget* parent, int score) {
-   background = new QLabel(this);
-   background->setPixmap(QPixmap(imagePrefixPath + backgroundPath));
-   background->setScaledContents(true);
-   background->setGeometry(QRect(0, 0, WIDTH ,HEIGHT));
-   fontId =  QFontDatabase::addApplicationFont(fontPath);
-   QStringList fontFamilies = QFontDatabase::applicationFontFamilies(fontId);
-   font = new QFont(fontFamilies.at(0),30);
-   setFont(*font);
-   yourNameLabel = new QLabel(this);
-   yourNameLabel->setFont(*font);
-   yourNameLabel->setGeometry(QRect(100, 175, 200 ,70));
-   yourNameLabel->setText("your name: ");
-   yourNameLineEdit = new QLineEdit(this);
-   yourNameLineEdit->setStyleSheet(
-    "QLineEdit {"
-    "   background: transparent;"  // Прозрачный фон
-    "   border: none;"            // Убираем рамку
-    "   color: black;"           // Цвет текста (можно изменить)
-    "}"
-   );
+LoseDialog::LoseDialog(QWidget *parent, int score) {
+    background = new QLabel(this);
+    background->setPixmap(QPixmap(imagePrefixPath + backgroundPath));
+    background->setScaledContents(true);
+    background->setGeometry(QRect(0, 0, WIDTH, HEIGHT));
+    fontId = QFontDatabase::addApplicationFont(fontPath);
+    QStringList fontFamilies = QFontDatabase::applicationFontFamilies(fontId);
+    font = new QFont(fontFamilies.at(0), 30);
+    setFont(*font);
+    yourNameLabel = new QLabel(this);
+    yourNameLabel->setFont(*font);
+    yourNameLabel->setGeometry(QRect(100, 175, 200, 70));
+    yourNameLabel->setText("your name: ");
+    yourNameLineEdit = new QLineEdit(this);
+    yourNameLineEdit->setStyleSheet(
+        "QLineEdit {"
+        "   background: transparent;" // Прозрачный фон
+        "   border: none;" // Убираем рамку
+        "   color: black;" // Цвет текста (можно изменить)
+        "}"
+    );
     yourNameLineEdit->setFont(*font);
-    yourNameLineEdit->setGeometry(275,175,200,70);
+    yourNameLineEdit->setGeometry(275, 175, 200, 70);
 
-   yourScoreLabel = new QLabel(this);
-   yourScoreLabel->setFont(*font);
-   yourScoreLabel->setGeometry(QRect(100, 225, 300 ,70));
-   yourScoreLabel->setScaledContents(true);
-   QString scoreString = "your score: " + QString::number(score);
-   yourScoreLabel->setText(scoreString);
+    yourScoreLabel = new QLabel(this);
+    yourScoreLabel->setFont(*font);
+    yourScoreLabel->setGeometry(QRect(100, 225, 300, 70));
+    yourScoreLabel->setScaledContents(true);
+    QString scoreString = "your score: " + QString::number(score);
+    yourScoreLabel->setText(scoreString);
+    saveButtonImagePath = "done.png";
+    saveButton = new QPushButton(this);
+    saveButton->setGeometry(QRect(100, 400, 112, 45));
 
+    cancelButtonImagePath = "cancel.png";
+    cancelButton = new QPushButton(this);
+
+    cancelButton->setGeometry(QRect(300, 400, 112, 45));
+
+    setDefaultStylnig(imagePrefixPath,saveButtonImagePath,saveButton);
+    setDefaultStylnig(imagePrefixPath,cancelButtonImagePath,cancelButton);
+}
+void LoseDialog::setDefaultStylnig(const QString &prefix, const QString &suffix, QPushButton *button) {
+    button->setStyleSheet(
+        "QPushButton {"
+        "   background-image: url(\"" + prefix + suffix + "\");"
+        "   background-repeat: no-repeat;" // Не повторять изображение
+        "   background-position: center;" // Центрировать
+        "   border: none;" // Убрать стандартную рамку
+        "   padding: 10px;" // Отступ для текста/иконки
+        "}"
+    );
 }
 LoseDialog::~LoseDialog() {
-   delete font;
+    delete font;
 }
--- gameVisualisation/LoseDialog.h
+++ gameVisualisation/LoseDialog.h
@@ -9,28 +9,33 @@
 #include <QLabel>
 #include <QLineEdit>
 #include <QFontDatabase>
-class LoseDialog  : public QDialog{
-    private:
-     const int HEIGHT = 500;
-     const int WIDTH = 500;
-     QLabel* background;
-     QLabel* yourScoreLabel;
-     QLabel* yourNameLabel;
-     QLineEdit* yourNameLineEdit;
-     QPushButton* saveButton;
-     QPushButton* cancelButton;
-     QString fontPath = "/home/radamir/CLionProjects/doubleJumper/requirments/DoodleJump.ttf";
-     QString imagePrefixPath = "requirments/Sprites/Doodle Jump/";
-     QString backgroundPath = "bck@2x.png";
+
+class LoseDialog : public QDialog {
+private:
+    const int HEIGHT = 500;
+    const int WIDTH = 500;
+    QLabel *background;
+    QLabel *yourScoreLabel;
+    QLabel *yourNameLabel;
+    QLineEdit *yourNameLineEdit;
+    QPushButton *saveButton;
+    QString saveButtonImagePath;
+    QPushButton *cancelButton;
+    QString cancelButtonImagePath;
+    QString fontPath = "/home/radamir/CLionProjects/doubleJumper/requirments/DoodleJump.ttf";
+    QString imagePrefixPath = "requirments/Sprites/Doodle Jump/";
+    QString backgroundPath = "bck@2x.png";
+
+    void setDefaultStylnig(const QString &prefix, const QString &suffix, QPushButton *button);
+
     int fontId;
-     QFont* font;
-     public:
-       LoseDialog(QWidget *parent,int score);
-        ~LoseDialog();
+    QFont *font;
 
+public:
+    LoseDialog(QWidget *parent, int score);
+
+    ~LoseDialog();
 };
-
-
 
 
 #endif //LOSEDIALOG_H
